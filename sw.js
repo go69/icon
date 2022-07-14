0dac94f18f7ef63220990cb3c6a0c60ce2a8ad07
vijay <svijay0123@gmail.com>
2022-07-14 23:11:15 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,54 @@
+const cacheName = "25foods"; // Change value to force update
+
+self.addEventListener("install", event => {
+	// Kick out the old service worker
+	self.skipWaiting();
+
+	event.waitUntil(
+		caches.open(cacheName).then(cache => {
+			return cache.addAll([
+				"/",
+				"favicon.ico", // Favicon, IE and fallback for other browsers
+        "https://www.25foods.com",
+			
+				"https://cdn.jsdelivr.net/gh/go69/icon@svg/25-foods-96.png", // Logo
+        "https://cdn.jsdelivr.net/gh/go69/icon@svg/25-foods-512.png", // Logo
+        				
+				"https://cdn.jsdelivr.net/gh/go69/icon@svg/manifest.webmanifest" // Manifest file
+				
+			]);
+		})
+	);
+});
+
+self.addEventListener("activate", event => {
+	// Delete any non-current cache
+	event.waitUntil(
+		caches.keys().then(keys => {
+			Promise.all(
+				keys.map(key => {
+					if (![cacheName].includes(key)) {
+						return caches.delete(key);
+					}
+				})
+			)
+		})
+	);
+});
+
+// Offline-first, cache-first strategy
+// Kick off two asynchronous requests, one to the cache and one to the network
+// If there's a cached version available, use it, but fetch an update for next time.
+// Gets data on screen as quickly as possible, then updates once the network has returned the latest data. 
+self.addEventListener("fetch", event => {
+	event.respondWith(
+		caches.open(cacheName).then(cache => {
+			return cache.match(event.request).then(response => {
+				return response || fetch(event.request).then(networkResponse => {
+					cache.put(event.request, networkResponse.clone());
+					return networkResponse;
+				});
+			})
+		})
+	);
+});
